86e96a2aaced29d5a16cbd431d3e32f311cc4686
gusta <gustavo_o_@hotmail.com>
2022-01-31 23:25:45 -0400

    Initial commit

---
+++ operador_walrus.py
@@ -0,0 +1,26 @@
+"""
+Operador Walrus
+"""
+
+# Antes
+#
+# nome = "Gustavo"
+# print(nome)
+#
+# # Com o Walrius
+#
+# print(nome := "Gustavo")
+
+
+# Python 3.7
+# cesta = []
+# fruta = input("Informe a fruta: ")
+# while fruta != "jaca":
+#     cesta.append(fruta)
+#     fruta = input("Informe a fruta: ")
+
+
+# Python 3.8 com Walrus
+cesta = []
+while (fruta := input("Informe a fruta: "))  != "jaca":
+    cesta.append(fruta)
