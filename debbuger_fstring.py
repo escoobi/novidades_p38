0d8749d843dc69738e17b3e269ee99d8fdc00196
gusta <gustavo_o_@hotmail.com>
2022-02-01 11:13:17 -0400

    Debug com f-string

---
+++ debbuger_fstring.py
@@ -0,0 +1,21 @@
+"""
+Debug com f-string
+"""
+
+#
+# def multiplicar(num1: float, num2: float) -> float:
+#     return num1 * num2
+#
+#
+# result: float = multiplicar(5.5, 8.9)
+#
+# print(f"O resultado é: {result}")
+# print(f"O resultado é: {multiplicar(8.9, 7.2):.2f}")
+#
+# print(f"{(media:= 8 / 2)} é a metade de {media * 2}")
+
+v1: str = "Gustavo"
+
+print(f"gustavo = '{v1}'")
+print(f"{v1=}")
+print(f"{v1.upper()[::-1] = }")
